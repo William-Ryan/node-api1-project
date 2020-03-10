1f00098b91ab80b96459a3ff0c63b7c30e86823c
William <billy.ryan.rom@gmail.com>
2020-03-10 11:12:54 -0400

    Add statuses to server.get and post for users/Add server.get for specific users with id

--- index.js
+++ index.js
@@ -24,11 +24,29 @@ server.post('/api/users', (req, res) => {
         users.push(userInfo);
 
         res.status(201).json(userInfo);
-    } else {
+    } else if (!userInfo.name || !userInfo.bio){
         res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
     }
 })
 
 server.get('/api/users', (req, res) => {
-    res.status(200).json(users);
+
+    if (users){
+        res.status(200).json(users);
+    } else {
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    }
+})
+
+server.get('/api/users/:id', (req, res) => {
+    
+    if (users.id){
+        res.status(200).json(users.id)
+    } else if (!users.id) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+    }
 })
