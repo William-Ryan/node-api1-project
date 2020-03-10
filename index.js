3099db82e5ef7ed83f776b361b543d2b91b4e2ca
William <billy.ryan.rom@gmail.com>
2020-03-10 10:48:22 -0400

    Add PORT 5000 to index.js/Add server commands to index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const shortid = require("shortid");
+
+const server = express()
+
+const PORT = 5000;
+server.listen(PORT, () => 
+    console.log(`\n ** API on http://localhost:${PORT} ** \n`)
+);
